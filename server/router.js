f9c5856f3b7efcc0048eac0fcbdfbc5fdce0cb9c
Calvin To <cxt6655@rit.edu>
2019-11-08 18:31:14 -0500

    fix type in router

--- server/router.js
+++ server/router.js
@@ -3,16 +3,16 @@ const middle = require('./middleware');
 
 // configure routes
 const router = (app) => {
-  app.get('/login', middle.requireSecure, middle.requiresLogout, controllers.Account.loginPage);
+  app.get('/login', middle.requiresSecure, middle.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', middle.requiresSecure, middle.requiresLogout, controllers.Account.login);
   app.get('/getDomos', middle.requiresLogin, controllers.Domo.getDomos);
   app.delete('/removeDomo', middle.requiresLogin, controllers.Domo.removeDomo);
-  app.post('/login', middle.requireSecure, middle.requiresLogout, controllers.Account.login);
-  app.get('/getToken', middle.requireSecure, controllers.Account.getToken);
-  app.post('/signup', middle.requireSecure, middle.requiresLogout, controllers.Account.signup);
+  app.get('/getToken', middle.requiresSecure, controllers.Account.getToken);
+  app.post('/signup', middle.requiresSecure, middle.requiresLogout, controllers.Account.signup);
   app.get('/logout', middle.requiresLogin, controllers.Account.logout);
   app.get('/maker', middle.requiresLogin, controllers.Domo.makerPage);
   app.post('/maker', middle.requiresLogin, controllers.Domo.make);
-  app.get('/', middle.requireSecure, middle.requiresLogout, controllers.Account.loginPage);
+  app.get('/', middle.requiresSecure, middle.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
